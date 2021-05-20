656048aacede92b78634ee92fe7c3854745116df
marxxcodes <marxx171@gmail.com>
2021-05-19 21:43:54 -0300

    Desafio 5 Vnw

---
+++ script.js
@@ -0,0 +1,14 @@
+let s = ['2 hamburgueres', 'alface', 'queijo', 'molho especial', 'cebola']
+console.log(s)
+s.unshift('pão')
+console.log(s)
+s.pop('cebola')
+console.log(s)
+s.push('cebola', 'picles')
+console.log(s)
+s.shift('pão')
+console.log(s)
+
+const numbers = [7,5,6,3,8,9,2,1,4]
+numbers.sort()
+console.log(numbers)
